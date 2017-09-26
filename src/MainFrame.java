5a801a5ac005e4cde137753320621bcf6b516d32
rtfgvb74125 <105021056@liva.asia.edu.tw>
2017-09-26 11:07:31 +0800

    ex2

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,14 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame{
+    private Button btnAdd = new Button("Add");
+    private Button btnExit = new Button("Exit");
+    private Label lab = new Label("0_0");
+    private int x = 0;
     public MainFrame(){
         init();
     }
@@ -11,6 +17,27 @@ public class MainFrame extends Frame{
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        btnAdd.setBounds(100,100,100,100);
+        btnExit.setBounds(150,100,100,100);
+        lab.setBounds(200,100,100,100);
+        this.add(btnAdd);
+        this.add(btnExit);
+        this.add(lab);
+
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.setTitle(Integer.toString(++x));
+                lab.setText(Integer.toString(x));
+            }
+        });
+
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
